202055dc3c34b0ea1f636885fbd3e1c7e3aaea4d
Rich Moss <richmoss@users.noreply.github.com>
2015-12-22 19:39:13 +0000

    Added popout/slideshow button using lightbox2

--- src/content/inject.js
+++ src/content/inject.js
@@ -1,85 +1,125 @@
-function InsertDownloadLinks() {
-	$(".thumb-listing-page").each(function(i) {
-		// don't add the links to the same thumbnails more than once
-		if ($(this).data("ewd-download-added") === true)
-			return true;
+(function WallhavenExtenderExtension() {
+	function InsertLinks() {
+		$(".thumb-listing-page").each(function(i) {
+			// don't add the links to the same thumbnails more than once
+			if ($(this).data("wee-download-added") === true)
+				return true;
 
-		$(this).find(".thumb-info").each(function(i) {
-			var id = $(this).parent("figure").data("wallpaper-id");
+			$(this).find(".thumb-info").each(function(i) {
+				var wallID = $(this).parent("figure").data("wallpaper-id");
+				var wallRes = $(this).children(".wall-res").eq(0).text();
+				var wallFavs = $(this).children(".wall-favs").eq(0).text();
 
-			$(this).append(
-				$("<a class='download-link'></a>")
+				$(this).append($("<a class='wee-download-link'></a>")
 					.prop({
-						"href":"http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-" + id + ".jpg",
-						"download":"wallhaven-" + id + ".jpg",
-						"title":"Download"
+						"href": WallpaperURL(wallID) + ".jpg",
+						"download": "wallhaven-" + wallID + ".jpg",
+						"title": "Download"
 					})
 					.css({
-						"right":"30px",
-						"position":"absolute"
+						"right": "30px",
+						"position": "absolute"
 					})
 					.click(function(e) {
-						// if we have already checked file type, drop out and let the download continue as normal
-						if (typeof $(this).data("ewd-has-type") !== "undefined") 
-							return;
-
-						e.preventDefault();
-
-						var anchor = this;
-
-						// check if the file exists as a jpg
-						$.ajax({
-							method: "HEAD",
-							url: "http://alpha.wallhaven.cc/wallpapers/full/wallhaven-" + id + ".jpg",
-							success: function() {
-								// jpg
-							},
-							error: function() {
-								// png
-								$(anchor).prop("href", "http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-" + id + ".png");
-							},
-							complete: function() {
-								$(anchor).data("ewd-has-type", true);
-								anchor.click();
-							}
-						});
+						// stop the click if we need to validate the file type
+						if (!ValidateFileType($(this), wallID)) {
+							e.preventDefault();
+						}
 					})
 					.tipsy({delayIn:500,delayOut:500,fade:!0})
 					.append("<i class='fa fa-download'></i>")
-			);
-		});
-
-		var downloadAll = $("<a href='#'></a>")
-			.prop({
-				"title":"Download page"
-			})
-			.css({
-				"margin-left":"10px"
-			})
-			.click(function(e) {
-				$(this).parents(".thumb-listing-page").eq(0).find(".download-link").each(function() {
-					this.click();
-				});
-				
-				e.preventDefault();
-			})
-			.tipsy({delayIn:500,delayOut:500,fade:!0});
-
-		downloadAll.append("<i class='fa fa-download'></i>");
-
-		$(this).children("header").eq(0).append(downloadAll);
-
-		$(this).data("ewd-download-added", true);
-	})
-}
-
-InsertDownloadLinks();
+				);
 
 
-$(document).ajaxComplete(function(event, xhr, settings) { 
-	//console.log(event);
-	//console.log(xhr);
-	//console.log(settings);
+				$(this).append($("<a></a>")
+					.prop({
+						"href": WallpaperURL(wallID) + ".jpg",
+						"title": "Popup",
+					})
+					.attr({
+						"data-lightbox": "wee-image",
+						"data-title": wallRes + " - Favorites: " + wallFavs
+					})
+					.css({
+						"right": "50px",
+						"position": "absolute"
+					})
+					.click(function(e) {
+						// if we need to validate the file type then block the click event
+						if (!ValidateFileType($(this), wallID)) {
+							e.preventDefault();
+						}
+					})
+					.tipsy({delayIn:500,delayOut:500,fade:!0})
+					.append("<i class='fa fa-expand'></i>")
+				);
+			});
 
-	InsertDownloadLinks();
-});
+			var downloadAll = $("<a href='#'></a>")
+				.prop({
+					"title": "Download page"
+				})
+				.css({
+					"margin-left": "10px"
+				})
+				.click(function(e) {
+					// find each individual download link that we created and click them
+					$(this).parents(".thumb-listing-page").eq(0).find(".wee-download-link").each(function() {
+						this.click();
+					});
+					
+					e.preventDefault();
+				})
+				.tipsy({delayIn:500,delayOut:500,fade:!0})
+				.append("<i class='fa fa-download'></i>");
+
+			$(this).children("header").eq(0).append(downloadAll);
+
+			// mark this page so it isn't processed again
+			$(this).data("wee-download-added", true);
+		})
+	}
+
+
+
+	function ValidateFileType(anchor, id) {
+		// don't check more than once
+		if (typeof anchor.data("wee-has-type") !== "undefined") 
+			return true;
+
+		// check if the file exists as a jpg
+		$.ajax({
+			method: "HEAD",
+			// use alpha. instead of wallpapers. to avoid any cross-origin business
+			url: "http://alpha.wallhaven.cc/wallpapers/full/wallhaven-" + id + ".jpg",
+			success: function() {
+				// jpg
+			},
+			error: function() {
+				// png
+				anchor.prop("href", WallpaperURL(id) + ".png");
+			},
+			complete: function() {
+				anchor.data("wee-has-type", true);
+				// simulate a real click (not a jquery click)
+				anchor[0].click();
+			}
+		});	
+
+		return false;
+	}
+
+	function WallpaperURL(id) {
+		return "http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-" + id;
+	}
+
+	$(document).ajaxComplete(function(event, xhr, settings) { 
+		//console.log(event);
+		//console.log(xhr);
+		//console.log(settings);
+
+		InsertLinks();
+	});
+
+	InsertLinks();
+})();
